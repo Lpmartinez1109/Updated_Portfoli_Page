9bae392a7e629f1db862448ca1e91c31c28d8bed
LPMartinez <lpmartinez1109@gmail.com>
2020-01-16 09:41:04 -0600

    Made fixes to pages

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-var WEBGL= "https://github.com/mrdoob/three.js/blob/master/examples/jsm/WebGL.js"
-// var ext = gl.getExtension("OES_texture_float");
+function openNav(){
+	document.getElementById("sidebar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+console.log("click works");
 
-// console.log(ext);
-
-// $(".banner").ripples({
-//         dropRadius: 10,
-//     perturbance: 0.03
-// });
-if ( WEBGL.isWebGLAvailable() ) {
-
-	// Initiate function or other initializations here
-	animate();
-
-} else {
-
-	var warning = WEBGL.getWebGLErrorMessage();
-	document.getElementById( 'container' ).appendChild( warning );
-
+}
+function closeNav(){
+	document.getElementById("sidebar").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
 }
